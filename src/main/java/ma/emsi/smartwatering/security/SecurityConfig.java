7f2da27c9b25a12f4e29b36f4e7ddb6c91ed3b34
Akram Fouguir <akramfgr16@gmail.com>
2023-10-21 17:17:24 +0100

    Modif SecurityConfig 21-10

--- src/main/java/ma/emsi/smartwatering/security/SecurityConfig.java
+++ src/main/java/ma/emsi/smartwatering/security/SecurityConfig.java
@@ -15,14 +15,14 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import ma.emsi.smartwatering.service.AppUserService;
 
-@Configuration @EnableWebSecurity 
+@Configuration @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	
+
 	@Autowired
 	AppUserService userService;
 	@Autowired
 	PasswordEncoder passwordEncoder;
- 
+
 	@Bean
 	public DaoAuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
@@ -30,36 +30,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		auth.setPasswordEncoder(passwordEncoder);
 		return auth;
 	}
-	
+
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws
-	Exception {
+			Exception {
 		auth.authenticationProvider(authenticationProvider());
 	}
-	
-	 @Override
-	protected void configure(HttpSecurity http) throws Exception {
-		 http.formLogin().defaultSuccessUrl("/", true);
-			http.httpBasic()
-		 .authenticationEntryPoint(new NoPopupBasicAuthenticationEntryPoint())
-		 .and().authorizeRequests()
-		 	.mvcMatchers("/inscription","/register", "/plantes", "/uploads", "/grandeurs/**", "/uploads/**","/","/login", "/logout", "/images/**", "/vendor/**",
-		 			"/js/**", "/bundles/**", "/charts/**", "/vendor/**", "/css/**").permitAll()
-		 	.mvcMatchers("/plantes",  "/zones/grandeurs/**","/realtime/**", "/farmer","/plantes/**", "/farmer/**", "/api/farmer/**").hasRole("USER")
-					.mvcMatchers("/users/update/**","/plantes/new").access("hasRole('USER') or hasRole('ADMIN')")
 
-		 	.mvcMatchers("/plantes", "/zones/grandeurs/**", "/plantes/**","/**", "/api/**").hasRole("ADMIN")
-		 	.anyRequest().authenticated()
-		 	.and()
-		 	.formLogin().loginPage("/login")
-		 	.and()
-		 	.logout()
-		 	.clearAuthentication(true).invalidateHttpSession(true)
-		 	.and()
-		 	.csrf().disable();
-	 }
-	 
-	 
-	 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.formLogin().defaultSuccessUrl("/", true);
+		http.httpBasic()
+				.authenticationEntryPoint(new NoPopupBasicAuthenticationEntryPoint())
+				.and().authorizeRequests()
+				.mvcMatchers("/inscription","/register", "/plantes", "/uploads", "/grandeurs/**", "/uploads/**","/","/login", "/logout", "/images/**", "/vendor/**",
+						"/js/**", "/bundles/**", "/charts/**", "/vendor/**", "/css/**").permitAll()
+				.mvcMatchers(  "/zones/grandeurs/**","/realtime/**", "/farmer", "/farmer/**", "/api/farmer/**").hasRole("USER")
+				.mvcMatchers("/users/update/**","/plantes","/plantes/new","/plantes/**").access("hasRole('USER') or hasRole('ADMIN')")
+
+				.mvcMatchers("/zones/grandeurs/**", "/**", "/api/**").hasRole("ADMIN")
+				.anyRequest().authenticated()
+				.and()
+				.formLogin().loginPage("/login")
+				.and()
+				.logout()
+				.clearAuthentication(true).invalidateHttpSession(true)
+				.and()
+				.csrf().disable();
+	}
+
+
+
 
 }
